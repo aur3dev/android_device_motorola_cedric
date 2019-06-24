57c68802e0ff8ad2a00729400f3c465e5a35bc72
aur3dev <aurelien.androidjb@gmail.com>
2019-06-24 23:04:58 +0200

    Update and rename lineage.mk to cm.mk
    change lineage to cm

--- cm.mk
+++ cm.mk
@@ -31,6 +31,6 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cedric
-PRODUCT_NAME := lineage_cedric
+PRODUCT_NAME := cm_cedric
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
